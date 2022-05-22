410cab44360219740fad1aaf6150b4a7eee68cac
Dana Janoskova <>
2022-05-22 19:29:55 +0200

    commit 8

--- example/src/components/Code/Code.tsx
+++ example/src/components/Code/Code.tsx
@@ -1,12 +1,15 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+import editorStyle from './style';
 
 import styles from './Code.module.scss';
 
-const Code: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
+const Code: FunctionComponent<{ children: string }> = ({ children }) => {
   return (
-    <div className={styles.content}>
-      <pre className={styles.code}>{children}</pre>
-    </div>
+    <SyntaxHighlighter className={styles.content} language="javascript" style={editorStyle as any}>
+      {children}
+    </SyntaxHighlighter>
   )
 }
 
---
+++ example/src/components/Code/style.ts
@@ -0,0 +1,98 @@
+const style = {
+  "hljs-comment": {
+    "color": "#7285b7"
+  },
+  "hljs-quote": {
+    "color": "#7285b7"
+  },
+  "hljs-variable": {
+    "color": "#9db6ff"
+  },
+  "hljs-template-variable": {
+    "color": "#9db6ff"
+  },
+  "hljs-tag": {
+    "color": "#ffffff"
+  },
+  "hljs-name": {
+    "color": "#9e9bff"
+  },
+  "hljs-selector-id": {
+    "color": "#b833fc"
+  },
+  "hljs-selector-class": {
+    "color": "#3a61ce"
+  },
+  "hljs-regexp": {
+    "color": "#2346a9"
+  },
+  "hljs-deletion": {
+    "color": "#294caf"
+  },
+  "hljs-number": {
+    "color": "#ffc58f"
+  },
+  "hljs-built_in": {
+    "color": "#ffc58f"
+  },
+  "hljs-builtin-name": {
+    "color": "#ffc58f"
+  },
+  "hljs-literal": {
+    "color": "#ffc58f"
+  },
+  "hljs-type": {
+    "color": "#ffc58f"
+  },
+  "hljs-params": {
+    "color": "#ffc58f"
+  },
+  "hljs-meta": {
+    "color": "#ffc58f"
+  },
+  "hljs-link": {
+    "color": "#ffc58f"
+  },
+  "hljs-attribute": {
+    "color": "#ffeead"
+  },
+  "hljs-string": {
+    "color": "#5effb5"
+  },
+  "hljs-symbol": {
+    "color": "#5effb5"
+  },
+  "hljs-bullet": {
+    "color": "#5effb5"
+  },
+  "hljs-addition": {
+    "color": "#5effb5"
+  },
+  "hljs-title": {
+    "color": "#bbdaff"
+  },
+  "hljs-section": {
+    "color": "#bbdaff"
+  },
+  "hljs-keyword": {
+    "color": "#ebbbff"
+  },
+  "hljs-selector-tag": {
+    "color": "#ebbbff"
+  },
+  "hljs": {
+    "display": "block",
+    "overflowX": "auto",
+    "background": "#002451",
+    "color": "white",
+    "padding": "0.5em"
+  },
+  "hljs-emphasis": {
+    "fontStyle": "italic"
+  },
+  "hljs-strong": {
+    "fontWeight": "bold"
+  }
+};
+
+export default style;
